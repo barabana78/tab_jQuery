c25ddcf931f2c5a9b80c853c478e12b3c5254bb8
AndruB <barabana78@gmail.com>
2019-02-18 22:59:31 +0200

    dz2

---
+++ main.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+$(document).ready(function(){
+	
+	$('.tab').click(function(){
+		var tab_id = $(this).attr('data-tab');//в кликнутом табе(объекте) находим номер по атрибуту
+
+		$('.tab').removeClass('active');//всем табам удаляем класс актив
+		$('.tabContent').addClass('hide');//скрываем весь контент добавляя класс hide
+		$(this).addClass('active');//кликнутой табке добавили актив
+		$("#"+tab_id).removeClass('hide');//вкладку для кликнутой табки открываем на просмотр удалив hide
+	})
+});
